e678c4b26741881028e16201cef73f0c73b51b08
Michel Pessoa Keursten <michelpessoa@gmail.com>
2020-09-18 18:20:55 -0300

    Update settings.py

--- mycalendar/settings.py
+++ mycalendar/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mql^z1ic+%wzc4_rli9jgl2yqdg_ob+5l-)3gb2rraumksj0i@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
